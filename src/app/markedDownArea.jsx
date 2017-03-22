d50d8550f7f6b8ca9ac25e5153f975a7b93bae13
Roky <Rock-son@users.noreply.github.com>
2017-03-22 01:47:57 +0100

    Delete markedDownArea.jsx

--- src/app/markedDownArea.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (properties) => {
-        const {className, children, ...props} = properties;
-        return (<div className={className}>
-                    <div {...props} dangerouslySetInnerHTML={{__html: children}}/>
-                </div>
-        );
-}
