129591ce8f0558f79d36d51365838a8ccd8a3f12
Rok <roky.zz@gmail.com>
2017-03-23 00:30:49 +0100

    final commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import './style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MarkedDownApp from './app/App';
+import MarkDownApp from './app/App';
 import {starterText} from './app/starterText';
 
 
-ReactDOM.render(<MarkedDownApp value={starterText}/>, document.getElementById('root'));
+ReactDOM.render(<MarkDownApp value={starterText}/>, document.getElementById('root'));
