3a8f8b838eb0f9a64262cb2388f200e18c3f94b2
Rok Z <roky.zz@gmail.com>
2017-04-05 23:10:14 +0200

    added plugin for hashing and templating

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.join(__dirname, 'build');
 var APP_DIR = path.join(__dirname, 'src');
@@ -15,8 +16,7 @@ var config = {
     },
     output: {
         path: BUILD_DIR,
-        filename: '[name].js',
-        publicPath: path.join(__dirname, 'public')
+        filename: '[name].[chunkhash].js'
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -42,7 +42,11 @@ var config = {
     plugins: [
         new ExtractTextPlugin({filename: 'style.css'}),
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: '../index.html'
         })
     ]
 }
